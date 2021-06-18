715a2bb0aff296ae2a92adc852a11b74b8050621
Ricardo Antonio Valladares Renderos <R_A_V_R_@HOTMAIL.COM>
2021-06-18 08:42:34 -0500

    Update Persona.h

--- Persona.h
+++ Persona.h
@@ -1,27 +1,27 @@
 /************************************************************************ 
-* usar funcion con argumentos:											*
-*																		*
-*	variable X;															*
-*	funcion(X); //es una funcion que solo lee la variable X				*
-*	funcion(&X); //es una funcion que lee y modifica la variable X		*
-*																		*
-*	arreglo X[10];														*
-*	funcion(X); //es una funcion que lee y modifica es arreglo X 		*
-*																		*
+* usar funcion con argumentos:						*
+*									*
+*	variable X;							*
+*	funcion(X); //es una funcion que solo lee la variable X		*
+*	funcion(&X); //es una funcion que lee y modifica la variable X	*
+*									*
+*	arreglo X[10];							*
+*	funcion(X); //es una funcion que lee y modifica es arreglo X 	*
+*									*
 ************************************************************************/
 /************************************************************************ 
-* construir funcion con argumentos:										*
-*																		*
-*	funcion(variable X){												*
-*		read X;				//solo lee variable X						*
-*	}																	*
-*	funcion(variable *X){												*
-*		*X = *X + 1;		//lee y modifica la variable X				*	
-*	}																	*
-*																		*
-*	funcion(arreglo X[]){												*
-*		X[0] = X[0] + 1;	//lee y modifica el arreglo X				*	
-*	}																	*
+* construir funcion con argumentos:					*
+*									*
+*	funcion(variable X){						*
+*		read X;				//solo lee variable X	*
+*	}								*
+*	funcion(variable *X){						*
+*		*X = *X + 1;		//lee y modifica la variable X	*	
+*	}								*
+*									*
+*	funcion(arreglo X[]){						*
+*		X[0] = X[0] + 1;	//lee y modifica el arreglo X	*	
+*	}								*
 ************************************************************************/
 
 
