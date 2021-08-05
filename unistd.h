eebf5c582f5b51ddf4f42ff79b433acbbcb1efe2
Ricardo Antonio Valladares Renderos <R_A_V_R_@HOTMAIL.COM>
2021-08-05 09:16:43 -0500

    Update unistd.h

--- unistd.h
+++ unistd.h
@@ -1,11 +1,19 @@
-#include <unistd.h> 
 #include <stdio.h> 
+#ifdef __TURBOC__ 
+#include <dos.h> 
+#else 
+#include <unistd.h> 
+#endif 
  
 void pausa(int seconds){ 
    fprintf(stderr,"Pausado... %d", seconds); 
-   do{ 
+   do{
       seconds--; 
+#ifdef __TURBOC__ 
+      delay(1000); 
+#else 
       sleep(1); 
+#endif
       fprintf(stderr," %d", seconds); 
    }while(seconds>0); 
    fprintf(stderr,"\n"); 
